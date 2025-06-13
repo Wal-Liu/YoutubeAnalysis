225dd605ed9cbd600ba1ed2f34a82938ce32b5d3
Wall Liu <tuongst689@gmail.com>
2025-06-13 22:10:20 +0700

    clean

--- DockerCompose/spark-apps/clean.py
+++ DockerCompose/spark-apps/clean.py
@@ -14,17 +14,36 @@ spark = SparkSession.builder \
     .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
     .getOrCreate()
 
-# Đọc dữ liệu từ Delta table ở bucket bronze
+
 raw_df = spark.read.format("delta").load("s3a://bronze/youtube/watch_history")
 
 
 # Làm phẳng dữ liệu nested JSON: explode_outer để giữ lại bản ghi kể cả khi subtitles rỗng/null
 flat_df = raw_df.withColumn("subtitle", explode_outer(col("subtitles")))
 
+
+# Bước 2: Chuẩn hóa thời gian
+# Chuyển 'time' string -> timestamp trước
+flat_df = flat_df.withColumn("timestamp", to_timestamp(col("time")))
+
+# Sau đó mới định dạng lại timestamp (loại bỏ milliseconds)
+flat_df = flat_df.withColumn(
+    "clean_timestamp", 
+    date_format(col("timestamp"), "yyyy-MM-dd HH:mm:ss")
+)
+
+
+# Xoá tiền tố "Đã xem " (có dấu cách) ở đầu title
+flat_df = flat_df.withColumn(
+    "clean_title",
+    regexp_replace(col("title"), "^Đã xem ", "")
+)
+
+
 # Chọn các cột cần thiết, lấy các trường con từ subtitle struct
 flat_df = flat_df.select(
-    col("time"),
-    col("title"),
+    col("clean_timestamp"),
+    col("clean_title"),
     col("titleUrl"),
     col("subtitle.name").alias("channel_name"),
     col("subtitle.url").alias("channel_url")
